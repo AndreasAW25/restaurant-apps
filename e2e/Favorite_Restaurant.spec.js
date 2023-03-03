6845163845bbd4daf8799378c198703262b1e612
Andreas <andreasandiw@gmail.com>
2023-03-03 13:13:20 +0700

    bug fixing test e2e unlike restaurant

--- e2e/Favorite_Restaurant.spec.js
+++ e2e/Favorite_Restaurant.spec.js
@@ -1,33 +1,66 @@
-const assert = require("assert");
+/* eslint-disable no-undef */
+const assert = require('assert');
 
-Feature("Favorite Restaurant");
+Feature('Favorite Restaurant');
 
 Before(({ I }) => {
-  I.amOnPage("/#/favorite");
+  I.amOnPage('/#/favorite');
 });
 
-Scenario("showing empty favorite restaurant", ({ I }) => {
-  I.seeElement("#notif-error");
-  I.see("Tidak ada restaurant untuk ditampilkan", "#notif-error");
+Scenario('showing empty favorite restaurant', ({ I }) => {
+  I.seeElement('#notif-error');
+  I.see('Tidak ada restaurant untuk ditampilkan', '#notif-error');
 });
 
-Scenario("liking one restaurant", async ({ I }) => {
-  I.see("Tidak ada restaurant untuk ditampilkan", "#notif-error");
+Scenario('liking one restaurant', async ({ I }) => {
+  I.see('Tidak ada restaurant untuk ditampilkan', '#notif-error');
 
-  I.amOnPage("/");
-  I.seeElement(".list-item-title a");
+  I.amOnPage('/');
+  I.seeElement('.list-item-title a');
 
-  const firstRestorant = locate(".list-item-title a").first();
+  const firstRestorant = locate('.list-item-title a').first();
 
   const firstRestorantTitle = await I.grabTextFrom(firstRestorant);
   I.click(firstRestorant);
 
-  I.seeElement("#likeButton");
-  I.click("#likeButton");
+  I.seeElement('#likeButton');
+  I.click('#likeButton');
 
-  I.amOnPage("/#/favorite");
-  I.seeElement(".list-item");
-  const likedFilmTitle = await I.grabTextFrom(".list-item-title");
+  I.amOnPage('/#/favorite');
+  I.seeElement('.list-item');
+  const likedRestorantTitle = await I.grabTextFrom('.list-item-title');
 
-  assert.strictEqual(firstRestorantTitle, likedFilmTitle);
+  assert.strictEqual(firstRestorantTitle, likedRestorantTitle);
+});
+
+Scenario('unliking one restaurant', async ({ I }) => {
+  I.see('Tidak ada restaurant untuk ditampilkan', '#notif-error');
+
+  I.amOnPage('/');
+  I.seeElement('.list-item-title a');
+
+  const firstRestorant1 = locate('.list-item-title a').first();
+  const firstRestorantTitle1 = await I.grabTextFrom(firstRestorant1);
+
+  I.click(firstRestorant1);
+
+  I.seeElement('#likeButton');
+  I.click('#likeButton');
+
+  I.amOnPage('/#/favorite');
+  I.seeElement('.list-item-title a');
+
+  const firstRestorant2 = locate('.list-item-title a').first();
+  const firstRestorantTitle2 = await I.grabTextFrom(firstRestorant2);
+  I.click(firstRestorant2);
+
+  assert.strictEqual(firstRestorantTitle1, firstRestorantTitle2);
+
+  I.seeElement('#likeButton');
+  I.click('#likeButton');
+
+  I.amOnPage('/#/favorite');
+  I.seeElement('#notif-error');
+  I.dontSeeElement('.list-item');
+  I.dontSeeElement('.list-item-title');
 });
